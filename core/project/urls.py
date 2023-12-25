5c703bc504a2f0bc57dc128c0c8eedd1f414d379
DaniFdz <danifernandezzzzzz@gmail.com>
2023-12-25 22:18:21 +0100

    Deleted unused urls

--- core/project/urls.py
+++ core/project/urls.py
@@ -6,9 +6,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
     path(settings.ADMIN_URL, admin.site.urls),
-    path("", include("core.home.urls")),
-    path("", include("core.shop.urls")),
-    path("", include("core.portfolio.urls")),
 ]
 
 if settings.DEBUG:
